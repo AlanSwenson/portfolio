798fee4842bf825f0daf5340aed1620a6c6ceb42
Alan Swenson <aswens0276@gmail.com>
2019-03-02 16:03:14 -0500

    add type.js

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 
+from portfolio.config import DevelopmentConfig
 
-def create_app():
+
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__, static_url_path="/static")
+    app.config.from_object(config_class)
 
     @app.route("/", methods=["POST", "GET"])
     def root():
